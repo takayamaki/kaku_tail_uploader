10f6757132765f4c4e901b9436662be17b8664b8
takayamaki <takayamaki@takayamaki.jp>
2018-10-09 06:59:32 +0900

    開発用の初期ユーザを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.development?
+  User.where(name: 'kakutail-admin').first_or_create(name: 'kakutail-admin',email: "kakutail-admin@localhost", password: 'kakutail-admin', password_confirmation: 'kakutail-admin', confirmed_at: Time.now.utc, role: :admin)
+  User.where(name: 'kakutail-user').first_or_create(name: 'kakutail-user',email: "kakutail-user@localhost", password: 'kakutail-user', password_confirmation: 'kakutail-user', confirmed_at: Time.now.utc, role: :creator)
+end
