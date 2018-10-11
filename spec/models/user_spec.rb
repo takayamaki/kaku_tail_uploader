ae162fccf576571dfc5b502d456c27b0b9025a5e
takayamaki <takayamaki@takayamaki.jp>
2018-10-11 11:21:11 +0900

    user_specを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,37 @@ describe User, type: :model do
     user = User.create(email: 'test@example.com', password: 'testPassword', name: 'test name', confirmed_at: Time.zone.now)
     expect(user).to be_valid
   end
+
+  context "returns role" do
+    it "when unauthorized" do
+      user = User.create(email: 'test@example.com', password: 'testPassword', name: 'test name', confirmed_at: Time.zone.now, role: 0)
+      expect(user.role).to eq "unauthorized"
+      expect(user.staff?).to eq false
+    end
+
+    it "when creator" do
+      user = User.create(email: 'test@example.com', password: 'testPassword', name: 'test name', confirmed_at: Time.zone.now, role: 1)
+      expect(user.role).to eq "creator"
+      expect(user.staff?).to eq false
+    end
+
+    it "when staff" do
+      user = User.create(email: 'test@example.com', password: 'testPassword', name: 'test name', confirmed_at: Time.zone.now, role: 2)
+      expect(user.role).to eq "staff"
+      expect(user.staff?).to eq true
+    end
+
+    it "when host" do
+      user = User.create(email: 'test@example.com', password: 'testPassword', name: 'test name', confirmed_at: Time.zone.now, role: 3)
+      expect(user.role).to eq "host"
+      expect(user.staff?).to eq true
+    end
+
+    it "when admin" do
+      user = User.create(email: 'test@example.com', password: 'testPassword', name: 'test name', confirmed_at: Time.zone.now, role: 4)
+      expect(user.role).to eq "admin"
+      expect(user.staff?).to eq true
+    end
+
+  end
 end
