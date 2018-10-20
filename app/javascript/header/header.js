b7ec2f3d8f5d56437d78059dc6b0c16dc18721ec
takayamaki <takayamaki@takayamaki.jp>
2018-10-20 20:47:34 +0900

    setToggleMenuHandler関数を別ファイルに切り出し

---
+++ app/javascript/header/header.js
@@ -0,0 +1,14 @@
+const toggleActive = (elm) =>{
+  elm.classList.contains('is-active') ? elm.classList.remove('is-active') : elm.classList.add('is-active');
+}
+
+export const setToggleMenuHandler = () =>{
+  const header = document.getElementById('header')
+  const menu = header.querySelector('.navbar-menu');
+  const burger = header.querySelector('.navbar-brand > .navbar-burger')
+
+  burger.addEventListener('click', ()=>{
+    toggleActive(menu)
+    toggleActive(burger)
+  })
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,19 +1,5 @@
 import '../styles/application'
-
-const toggleActive = (elm) =>{
-  elm.classList.contains('is-active') ? elm.classList.remove('is-active') : elm.classList.add('is-active');
-}
-
-const setToggleMenuHandler = () =>{
-  const header = document.getElementById('header')
-  const menu = header.querySelector('.navbar-menu');
-  const burger = header.querySelector('.navbar-brand > .navbar-burger')
-
-  burger.addEventListener('click', ()=>{
-    toggleActive(menu)
-    toggleActive(burger)
-  })
-}
+import { setToggleMenuHandler } from '../header/header'
 
 const run = ()=>{
   setToggleMenuHandler();
