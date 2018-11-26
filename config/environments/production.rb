3419502f8dbdcc106452e047874e550d8ea77b09
takayamaki <takayamaki@takayamaki.jp>
2018-11-27 03:24:33 +0900

    ドメイン名関連の設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: ENV.fetch(:DOMAIN_NAME) }
+  config.action_mailer.default_url_options = { host: ENV.fetch('DOMAIN_NAME') }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
