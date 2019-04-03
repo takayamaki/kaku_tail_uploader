ed205e5041ccff1be96ed07151ee1cb3af43d44f
takayamaki <takayamaki@takayamaki.jp>
2019-04-03 14:39:34 +0900

    登録時に名前を入力してくださいとのエラーが出て登録できなかった問題を修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_account_create_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
   protected
+  def configure_account_create_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
