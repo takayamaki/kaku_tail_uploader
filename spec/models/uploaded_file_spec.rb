8fec6ee24bec68632393511faf284f0fb15160dc
takayamaki <takayamaki@takayamaki.jp>
2019-05-05 02:27:26 +0900

    プレビューポイントのvalidation向けにテストを追加

--- spec/models/uploaded_file_spec.rb
+++ spec/models/uploaded_file_spec.rb
@@ -76,4 +76,160 @@ describe UploadedFile, type: :model do
       expect(uploaded_file).to_not be_valid
     end
   end
+
+  context "frame part of shorter section are greater than longer one" do
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4", user: user, file_data: '{}',
+        thumbnail_sec_part: 59, thumbnail_frame_part: 1,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:0,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to_not be_valid
+    end
+
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4",  user: user, file_data: '{}',
+        thumbnail_sec_part: 60, thumbnail_frame_part: 1,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:0,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to be_valid
+    end
+
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4", user: user, file_data: '{}',
+        thumbnail_sec_part: 61, thumbnail_frame_part: 1,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:0,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to be_valid
+    end
+
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4", user: user, file_data: '{}',
+        thumbnail_sec_part: 75, thumbnail_frame_part: 1,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:0,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to_not be_valid
+    end
+
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4", user: user, file_data: '{}',
+        thumbnail_sec_part: 76, thumbnail_frame_part: 1,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:0,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to_not be_valid
+    end
+  end
+
+  context "frame part of shorter section are less than longer one" do
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4", user: user, file_data: '{}',
+        thumbnail_sec_part: 59, thumbnail_frame_part: 0,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:1,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to_not be_valid
+    end
+
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4",  user: user, file_data: '{}',
+        thumbnail_sec_part: 60, thumbnail_frame_part: 0,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:1,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to_not be_valid
+    end
+
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4", user: user, file_data: '{}',
+        thumbnail_sec_part: 61, thumbnail_frame_part: 0,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:1,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to be_valid
+    end
+
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4", user: user, file_data: '{}',
+        thumbnail_sec_part: 75, thumbnail_frame_part: 0,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:1,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to be_valid
+    end
+
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4", user: user, file_data: '{}',
+        thumbnail_sec_part: 76, thumbnail_frame_part: 0,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:1,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to_not be_valid
+    end
+  end
+
+  context "frame part of shorter section are equal with longer one" do
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4", user: user, file_data: '{}',
+        thumbnail_sec_part: 59, thumbnail_frame_part: 0,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:0,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to_not be_valid
+    end
+
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4",  user: user, file_data: '{}',
+        thumbnail_sec_part: 60, thumbnail_frame_part: 0,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:0,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to be_valid
+    end
+
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4", user: user, file_data: '{}',
+        thumbnail_sec_part: 61, thumbnail_frame_part: 0,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:0,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to be_valid
+    end
+
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4", user: user, file_data: '{}',
+        thumbnail_sec_part: 75, thumbnail_frame_part: 0,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:0,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to be_valid
+    end
+
+    it do
+      uploaded_file = UploadedFile.create(
+        file_name: "test.mp4", user: user, file_data: '{}',
+        thumbnail_sec_part: 76, thumbnail_frame_part: 0,
+        start_of_15sec_sec_part: 60, start_of_15sec_frame_part:0,
+        start_of_30sec_sec_part: 60, start_of_30sec_frame_part:0,
+        start_of_60sec_sec_part: 60, start_of_60sec_frame_part:0)
+      expect(uploaded_file).to_not be_valid
+    end
+  end
 end
