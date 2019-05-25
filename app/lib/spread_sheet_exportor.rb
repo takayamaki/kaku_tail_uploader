506c9b8ba803ac64d6f1ce361cbab825648fab13
takayamaki <takayamaki@takayamaki.jp>
2019-05-25 16:39:27 +0900

    SpreadSheetExportorの認証情報の渡しかたを変更

--- app/lib/spread_sheet_exportor.rb
+++ app/lib/spread_sheet_exportor.rb
@@ -3,10 +3,19 @@ require 'singleton'
 
 class SpreadSheetExportor
   include Singleton
-  attr_writer :credentials, :defaultSpreadSheetName
+
+  def initialize
+    @credentials = Google::Auth::UserRefreshCredentials.new(
+      client_id:     Config.google_drive_client_id,
+      client_secret: Config.google_drive_client_secret,
+      refresh_token: Config.google_drive_refresh_token,
+      scope:         'https://www.googleapis.com/auth/drive.file',
+      redirect_uri:  'urn:ietf:wg:oauth:2.0:oob',
+    )
+    @defaultSpreadSheetName = Config.defaultSpreadSheetName
+  end
 
   def get_or_create_worksheet(workSheetName, spreadSheetName = @defaultSpreadSheetName.to_s)
-    raise 'Set defaultSpreadSheetName!!' unless @defaultSpreadSheetName
     spreadsheet = get_or_create_spreadsheet(spreadSheetName)
     @workSheets ||= {}
     @workSheets["#{spreadSheetName}/#{workSheetName}"] ||=
@@ -16,6 +25,7 @@ class SpreadSheetExportor
 
   private
   def create_session
+    GoogleDrive::Session.new_dummy if Rails.env.test?
     @credentials.fetch_access_token!
     GoogleDrive::Session.from_credentials(@credentials)
   end
